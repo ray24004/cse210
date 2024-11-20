c8daeea1c758fe90c4b47b33349ff8ace41732c7
ray24004 <ray24004@byui.edu>
2024-11-20 00:55:59 -0300

    Complete Learning 3 - Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6, 7);
+
+        Console.WriteLine($"first: {first.GetFractionString()} {first.GetDecimalValue()}");
+        Console.WriteLine($"first: {second.GetFractionString()} {second.GetDecimalValue()}");
+        Console.WriteLine($"first: {third.GetFractionString()} {third.GetDecimalValue()}");
+
+        first.SetTop(3);
+        first.SetBottom(6);
+        Console.WriteLine($"first: {first.GetFractionString()} {first.GetDecimalValue()}");
     }
 }
