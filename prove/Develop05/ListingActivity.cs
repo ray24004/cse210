82b982b4e418206992d8755122aee9021f7ecd7d
ray24004 <ray24004@byui.edu>
2024-12-04 22:21:02 -0300

    Add base design for W05 mindfullness

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,25 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    private void GetRandomPrompt()
+    {
+
+    }
+
+    private List<string> GetListFromUser()
+    {
+        return default;
+    }
+}
---
+++ prove/Develop05/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity : Activity
+{
+    public List<string> _prompts;
+    public List<string> _questions;
+
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    private string GetRandomPrompt()
+    {
+        return default;
+    }
+
+    private string GetRandomQuestion()
+    {
+        return default;
+    }
+
+    private void DisplayPrompt()
+    {
+
+    }
+
+    private void DisplayQuestions()
+    {
+        
+    }
+}
