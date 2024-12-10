d213d9c0d0ff64526b57cd6997a4bb916bf5155e
ray24004 <ray24004@byui.edu>
2024-12-09 23:49:10 -0300

    Complete W5 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine();
+
+        Assignment assignment = new Assignment("Wesley Raymundo", "Game Design");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Henrique Nascimento", "Game Physics", "7.1", "8-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Carlos Nasray", "Game History", "How the first videogame was created");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine();
     }
 }
